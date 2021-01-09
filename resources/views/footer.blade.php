a2b50027361657b9fe70fdcdbcb55bff5b89849b
Cristian <cmartmac@gmail.com>
2021-01-09 14:31:15 +0100

    Añadida una cabecera y un footer para la página

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,74 @@
+<!-- Footer -->
+<footer class="bg-dark text-center text-light text-lg-start fixed-bottom">
+	
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Sobre nosotros</h5>
+
+        <p>
+         Llevamos trabajando para el cuidado y el tratamiento de patologías
+		 fisiológicas y psicológicas desde 1963. Nuestros expertos están titulados
+		 en distintos campos desde fisioterapia hasta psicología para cumplir con las 
+		 necesidades de nuestros clientes.
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Enlaces de interés</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-light">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-light">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-light">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-light">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">&nbsp</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-light">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-light">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-light">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-light">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2021:
+    <a class="text-light" href="https://mdbootstrap.com/">Clínicas Fisioweb</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,39 +7,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
     </head>
-	
-	
 		<body>
-		<nav class="navbar navbar-expand-xs navbar-light bg-light">
-		  <a class="navbar-brand" href="#">Navbar</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		
+		<!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR -->
+	
+		
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <a class="navbar-brand" href="#">Clínicas Fisioweb</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		  </button>
-		  <div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
 			  <li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" href="#">Features</a>
+				<a class="nav-link" href="#">Tratamientos</a>
+			  </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  Dropdown
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				  <a class="dropdown-item" href="#">Action</a>
+				  <a class="dropdown-item" href="#">Another action</a>
+				  <div class="dropdown-divider"></div>
+				  <a class="dropdown-item" href="#">Something else here</a>
+				</div>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" href="#">Pricing</a>
+				<a class="nav-link disabled" href="#">En un futuro...</a>
 			  </li>
-			  <li class="nav-item">
-				<a class="nav-link disabled" href="#">Disabled</a>
+			  <li class=nav-item>
+			  <a class="nav-link" href="#">Reservar cita</a>
 			  </li>
 			</ul>
+			<form class="form-inline my-2 my-lg-0">
+			  <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+			  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+			</form>
 		  </div>
 		</nav>
 
-
+		<!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR -->
 
 
 		
@@ -53,21 +72,21 @@
 		  </ol>
 		  <div class="carousel-inner">
 			<div class="carousel-item active">
-			  <img class="d-block w-100" src="imagenes/carrusel1.jpg" alt="First slide" height=550px> <!-- Imagen 1 del carrusel -->
+			  <img class="d-block w-100 min-v-100" src="imagenes/carrusel1.jpg" height=680px> <!-- Imagen 1 del carrusel -->
 				  <div class="carousel-caption d-none d-md-block">
 					<h5>Clínicas Fisioweb</h5>
 					<p>Con tratamientos para todo tipo de problemas fisiológicos.</p>
 				  </div>
 			</div>
 			<div class="carousel-item">
-			  <img class="d-block w-100" src="imagenes/carrusel2.jpg" alt="Second slide" height=550px> <!-- Imagen 2 del carrusel -->
+			  <img class="d-block w-100" src="imagenes/carrusel2.jpg" height=680px> <!-- Imagen 2 del carrusel -->
 			  	  <div class="carousel-caption d-none d-md-block">
 					<h5>Tratamientos de aromaterapia</h5>
 					<p>Realizamos masajes y tratamientos con aromaterapia para tratar lesiones causadas por el estrés.</p>
 				  </div>
 			</div>
 			<div class="carousel-item">
-			  <img class="d-block w-100" src="imagenes/carrusel3.jpg" alt="Third slide" height=550px> <!-- Imagen 3 del carrusel -->
+			  <img class="d-block w-100" src="imagenes/carrusel3.jpg" height=680px> <!-- Imagen 3 del carrusel -->
 			  	  <div class="carousel-caption d-none d-md-block">
 					<h5>Rehabilitación completa con seguimiento del paciente</h5>
 					<p>En clínicas Fisioweb damos un seguimiento completo a cualquier cliente durante los años posteriores a su rehabilitación.</p>
@@ -86,11 +105,6 @@
 		
 		<!-- CARRUSEL --><!-- CARRUSEL --><!-- CARRUSEL --><!-- CARRUSEL --><!-- CARRUSEL --><!-- CARRUSEL --><!-- CARRUSEL --><!-- CARRUSEL -->
 		
-		<div>
-		<h5> Texto aleatorio</h5>
-		</div>
-
-		
 		@auth
 		<!-- Utilizar esta parte unicamente para cuando el usuario este logueado-->
 		@endauth
@@ -100,4 +114,5 @@
 		@endguest
 
 		</body>
+		@include('footer')
 </html>
