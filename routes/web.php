cbc6c43f4de600cd3204f3458f9e28ca6b3aff7e
Cristian <cmartmac@gmail.com>
2021-01-16 19:48:30 +0100

    Corregidas las rutas que habían sido modificadas por Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', function () {return view('Inicio');});
+Route::get('/Inicio', function () {return view('Inicio');});
+Route::get('/Profesionales', [TrabajadoresController::class, 'obtenerListadoTrabajadores']);
+Route::get('/Tarifas', function () {return view('Tarifas');});
+Route::get('/Reserva', function () {return view('Reserva');});
+
+
+
+// Tratamientos - Informacion estatica
+Route::get('/Fisioterapia', [TratamientosController::class, 'obtenerTratamientosFisioterapia']);
+Route::get('/Acupuntura', [TratamientosController::class, 'obtenerTratamientosAcupuntura']);
+Route::get('/Osteopatia', [TratamientosController::class, 'obtenerTratamientosOsteopatia']);
+Route::get('/AvisoLegal', function() {return view('static/AvisoLegal');});
+Route::get('/Politicas', function() {return view('static/Politicas');});
+Route::get('/TerminosyCondiciones', function() {return view('static/TerminosyCondiciones');});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
