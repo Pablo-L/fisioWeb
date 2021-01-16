38a551a24b1ac572dd58410e461b0b69b1ba76f5
Pablo <pld7@alu.ua.es>
2021-01-16 18:58:26 +0100

    controller vacío para reservas

---
+++ app/Http/Controllers/ReservasController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReservasController extends Controller
+{
+    //dado un id de cliente y uno de trabajador realiza una reserva
+
+    //dado un id de trabajador realiza una reserva de tiempo ocupado
+
+    //dado un id de trabajador muestra un listado de citas
+
+    //dado un id de cliente muestra el listado de citas del cliente
+
+    //dado un id de trabajador y una fecha devuelve un bool que determina si el día esta libre o no
+
+    //dado un id de trabajador y una fecha y una hora devuelve un bool que determina si esa hora esta disponible o no
+
+}
