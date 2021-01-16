1bc13954baf3a6c57b195e2515148d59907fbb12
Cristian <cmartmac@gmail.com>
2021-01-17 00:58:53 +0100

    Pequeño ajuste a la navbar con usuario normal.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -49,7 +49,7 @@
 				
 				@else
 				
-				<ul class="navbar-nav mr-auto" style="margin-left:65%">
+				<ul class="navbar-nav mr-auto" style="margin-left:64%">
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 					  {{ Auth::user()->name }}
