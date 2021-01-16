5f598ae8b82d292efe50691b8777dd400404532f
Cristian <cmartmac@gmail.com>
2021-01-16 20:29:05 +0100

    Añadido al navbar el nombre del usuario y un botón de perfil y logout

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -35,19 +35,26 @@
 			
 			
 			@auth <!-- Utilizar esta parte unicamente para cuando el usuario este logueado-->
-			<form class="form-inline my-2 my-lg-0">
-			  <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
-			  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-			</form>
+			<ul class="navbar-nav mr-auto" style="margin-left:65%">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  {{ Auth::user()->name }}
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				  <a class="dropdown-item" href="/profile">Perfil</a>
+				  <a class="dropdown-item" href="/logout">Cerrar sesión</a>
+				</div>
+			  </li>
+			  </ul>
 			@endauth
 
 			@guest <!-- Esta parte solo para visitantes -->
 			<ul class="navbar-nav mr-auto" style="margin-left:60%">
 			<li class="nav-item">
-				<a class="nav-link" href="#">Iniciar sesión</a>
+				<a class="nav-link" href="/login">Iniciar sesión</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Registrarse</a>
+				<a class="nav-link" href="/register">Registrarse</a>
 			</li>
 			<li>
 			</ul>
