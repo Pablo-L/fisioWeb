d4ae45cf2b52160178705c22d060f36cc8f851eb
Cristian <cmartmac@gmail.com>
2021-01-21 20:36:18 +0100

    Mejoras estéticas a la vista de Profesionales.

--- resources/views/Profesionales.blade.php
+++ resources/views/Profesionales.blade.php
@@ -21,18 +21,17 @@
 	}
 	@endphp
 	<div class="card">
-    <img style="height:438px; width:438px;"class="card-img-top" src="imagenes/{{$imgstring}}.jpg" alt="Imagen del/de la emprendedor/a">
-    <div class="card-body">
+    <img style="height:438px; min-width:auto;"class="card-img-top" src="imagenes/{{$imgstring}}.jpg">
+    <div class="card-body text-center">
       <h5 class="card-title text-center">{{$trabajador->nombre}}</h5>
+	  <hr class="my-4">
       <p class="card-text">Persona trabajadora, brillante y con un gran futuro por delante. Su gran vocación por la fisioterapía le ha llevado a trabajar con nosotros en Fisioweb y a desarrollar su carrera profesional creciendo poco a poco a nuestro lado.</p>
       <p class="card-text"><small class="text-muted">Trabajando con nosotros desde hace {{rand(1,14)}} años.</small></p>
-	  <button onclick="reservar('{{$trabajador->DNI}}')" >Rerservar</button>
+	  <button  class="btn btn-primary" onclick="reservar('{{$trabajador->DNI}}')">Solicitar cita</button>
 	</div>
   </div>
 	@endforeach
 </div>
-
-<div class=jumbotron></div>
 @section('js')
     function reservar(value) {
         console.log("Reserva/"+value);
