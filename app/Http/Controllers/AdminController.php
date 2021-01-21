05d520166bbf2be1670a0edf5c4c35c13b64a1e0
Cristian <cmartmac@gmail.com>
2021-01-21 06:05:34 +0100

    Añadida vista de Resumen de Panel de Administración y su logica .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Auth;
 use Illuminate\Http\Request;
 
 
+
 class AdminController extends Controller
 {
 	public function isAdmin()
@@ -27,7 +28,25 @@ class AdminController extends Controller
 	public function Resumen()
 	{
 		if(Auth::check() && Auth::user()->rol == "admin")
-			return view('admin/adminpanel_resumen');
+		{
+			
+			$numtrabajadores = \DB::table('trabajadores')->count();
+			$numusers = \DB::table('users')->count();
+			$numclientes = \DB::table('users')->where('rol', '!=', "admin")->where('rol', '!=', "recepcionista")->where('rol', '!=', "trabajador")->count();
+			$numaltasmes = \DB::table('users')->whereMonth('created_at', now()->month)->count();
+			$numaltasmes_pasado = \DB::table('users')->whereMonth('created_at', now()->month - 1)->count();
+			$numreservasmes = \DB::table('reservas')->whereMonth('dia', now()->month)->count();
+			$numreservasmes_pasado = \DB::table('reservas')->whereMonth('dia', now()->month - 1)->count();
+			$numtratamientos = \DB::table('tratamientos')->count();
+			$nombrescategorias = \DB::table('categorias')->pluck('nombre');
+			
+			
+			
+			
+			return view('admin/adminpanel_resumen', ['numero_Trabajadores' => $numtrabajadores, 'numero_usuarios' => $numusers, 'numero_clientes' => $numclientes, 'numero_altas_mes' => $numaltasmes, 
+			'numero_altas_mespasado' => $numaltasmes_pasado, 'numero_reservas_mes' => $numreservasmes, 'numero_reservas_mespasado' => $numreservasmes_pasado, 'numtratamientos' => $numtratamientos,
+			'nombrescategorias' => $nombrescategorias]);
+		}
 		else
 			return redirect('/login');
 	}
