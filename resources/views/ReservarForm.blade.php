9a499c71d5d75c1392878e7895c71965aeb20504
Pablo <pld7@alu.ua.es>
2021-01-22 13:23:46 +0100

    Estética formulario reservas

--- resources/views/ReservarForm.blade.php
+++ resources/views/ReservarForm.blade.php
@@ -9,72 +9,70 @@
     $arrayDias = $controlador->obtenerDiasSemana();
 ?>
 @section('contenido')
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <nav class="navbar navbar-expand-lg navba bg">
-		  <a class="navbar-brand" style="color:grey;">Haz tu reserva con {{$trabajador->obtenerNombre($trabajador_id)}}</a>
-		  </nav>
-        </x-slot>
+    <div class="container">
+		  <h2 style="color:grey;">Haz tu reserva con {{$trabajador->obtenerNombre($trabajador_id)}}</h2>
+
 
 		@if (\Session::has('error'))
 			<div class="alert alert-danger alert-dismissible fade show" role="alert">
-			{!! \Session::get('error') !!}
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-			</button>
+                {!! \Session::get('error') !!}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
 			</div>
 		@endif
+        <div style="align:center;">
+        <div style="background:grey;
+                    position:relative;
+                    display:table;
+                    padding:10px; 
+                    border-radius:10px;
+                    margin-bottom:25px;">
+            <form id="reservaForm" name="reservaForm" method="POST" action="{{route('reservar')}}">
+                @csrf
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-        
-        <form id="reservaForm" name="reservaForm" method="POST" action="{{route('reservar')}}">
-            @csrf
+                <!-- Tratamientos -->
+                <div class="mt-4">
+                    <label>Tratamiento</label></br>
+                    <select name="tratamiento_id" id="tratamiento_id" required>
+                    @foreach($tratamientos as $tratamiento)
+                    <option label="{{$tratamiento->nombre}}" value="{{$tratamiento->id}}"></option>
+                    @endforeach
+                    </select>
+                </div>
 
-            <!-- Tratamientos -->
-            <div class="mt-4">
-                <label>Tratamiento</label>
-                <select name="tratamiento_id" id="tratamiento_id" required>
-                @foreach($tratamientos as $tratamiento)
-                <option label="{{$tratamiento->nombre}}" value="{{$tratamiento->id}}"></option>
-                @endforeach
-                </select>
-            </div>
+                <!-- Días -->
+                <div class="mt-4">
+                    <label>Dia</label></br>
+                    <!-- Muestra en rojo los días no disponibles -->
+                    <select id="dia" name="dia">
+                    @foreach($arrayDias as $dia)
+                        @if( in_array($dia, $arrayLibres) )
+                        <option style="color:red">{{$dia}}</option>
+                        @else
+                        <option>{{$dia}}</option>
+                        @endif
+                    @endforeach
+                    </select>
+                </div>
 
-            <!-- Días -->
-            <div class="mt-4">
-                <x-label for="dia" :value="__('Dia')" />
-                <!-- Muestra en rojo los días no disponibles -->
-                <select id="dia" name="dia">
-                @foreach($arrayDias as $dia)
-                    @if( in_array($dia, $arrayLibres) )
-                    <option style="color:red">{{$dia}}</option>
-                    @else
-                    <option>{{$dia}}</option>
-                    @endif
-                @endforeach
-                </ul>
-            </div>
+                <!-- Hora -->
+                <div class="mt-4">
+                    <label>Hora</label></br>
+                    <x-input id="hora" class="block mt-1 w-full" type="time" name="hora" :value="old('hora')" required autofocus />
+                </div>
+                <!-- Campos ocultos -->
+                <x-input id="trabajador_id" type="hidden" name="trabajador_id" value="{{$trabajador_id}}"/>
+                <x-input id="cliente_id" type="hidden" name="cliente_id" value="{{Auth::user() ->id}}"/>
+                <span id="spanMensaje" style="color:green;"></span>
 
-            <!-- Hora -->
-            <div class="mt-4">
-                <label>Hora</label>
-                <x-input id="hora" class="block mt-1 w-full" type="time" name="hora" :value="old('hora')" required autofocus />
-            </div>
-            <!-- Campos ocultos -->
-            <x-input id="trabajador_id" type="hidden" name="trabajador_id" value="{{$trabajador_id}}"/>
-            <x-input id="cliente_id" type="hidden" name="cliente_id" value="{{Auth::user() ->id}}"/>
-            <span id="spanMensaje" style="color:green;"></span>
-
-            <!-- Botón reservar -->
-            <div class="flex items-center justify-end mt-4">
-                <input type="submit" value="Reservar cita" class="ml-3" onclick="">
-            </div>
-        </form>
-
-    </x-auth-card>
-</x-guest-layout>
+                <!-- Botón reservar -->
+                <div class="mt-4">
+                    <input type="submit" value="Reservar cita">
+                </div>
+            </form>
+        </div>
+    </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 @section('js')
     function reservar() {
